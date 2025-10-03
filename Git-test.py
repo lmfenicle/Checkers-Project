30d9c6aa6f5608a3862d8c27a57078454af21575
Jack <jdmeadows05@gmail.com>
2025-10-03 13:39:21 -0600

    Created the board

--- Git-test.py
+++ Git-test.py
@@ -5,16 +5,42 @@ import pygame
 pygame.init()
 
 #screen sizes
-width = 1200
+width = 800
 height = 800
 
 #create screen
 screen = pygame.display.set_mode((width, height))
 
+#colors
+GREEN  = (0, 255, 0)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+ORANGE = (255, 128, 0)
+YELLOW = (255, 255, 0)
+PURPLE = (128, 0, 255)
+
+backgroundColor = GREEN
+
+#running bool
 isRunning = True
 
 #start of running loop
 while isRunning:
+
+    #set color
+    screen.fill(backgroundColor)
+
+    #create board
+    for i in range(0,4):
+        for j in range(0,8):
+            if (j % 2 == 0):
+                pygame.draw.rect(screen, WHITE, (100 + 150*i, 100 + 75 * j, 75, 75), 0)
+                pygame.draw.rect(screen, BLACK, (175 + 150 * i, 100 + 75 * j, 75, 75), 0)
+            else:
+                pygame.draw.rect(screen, BLACK, (100 + 150 * i, 100 + 75 * j, 75, 75), 0)
+                pygame.draw.rect(screen, WHITE, (175 + 150 * i, 100 + 75 * j, 75, 75), 0)
 
     #event listener
     for event in pygame.event.get():
@@ -25,7 +51,10 @@ while isRunning:
             #quit game
             isRunning = False
 
+    #update screen
+    pygame.display.update()
+
 #end pygame
 pygame.quit()
 
-#push it real good
+
