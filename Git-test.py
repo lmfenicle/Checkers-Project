3f2782f69de4429b18b7449c797dec8754c9de3d
Jack <jdmeadows05@gmail.com>
2025-10-03 12:51:59 -0600

    Create game enviorment basics

--- Git-test.py
+++ Git-test.py
@@ -1,1 +1,29 @@
-print("Hello World")
+#imports
+import pygame
+
+#inizitlize pygame
+pygame.init()
+
+#screen sizes
+width = 800
+height = 600
+
+#create screen
+screen = pygame.display.set_mode((width, height))
+
+isRunning = True
+
+#start of running loop
+while isRunning:
+
+    #event listener
+    for event in pygame.event.get():
+
+        #x out of window
+        if event.type == pygame.QUIT:
+
+            #quit game
+            isRunning = False
+
+#end pygame
+pygame.quit()
